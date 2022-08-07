937f7f9dac3376220b7a974ea7a90d15783ccf8a
Deg2v <1311618499.com>
2022-08-07 22:36:24 +0800

    fit version1

--- custome.el
+++ custome.el
@@ -4,12 +4,19 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(awesome-tab-active-bar-height 51)
+ '(column-number-mode t)
+ '(custom-safe-themes
+   '("d14f3df28603e9517eb8fb7518b662d653b25b26e83bd8e129acea042b774298" "2078837f21ac3b0cc84167306fa1058e3199bbd12b6d5b56e3777a4125ff6851" default))
  '(package-selected-packages
-   '(magit monokai-theme monokai wgrep embark-consult embark orderless keycast consult marginalia vertico company))
+   '(all-the-icons org-roam compat company-flx doom-modeline lsp-bridge ox-latex magit monokai-theme monokai wgrep embark-consult embark orderless keycast consult marginalia vertico company))
+ '(size-indication-mode t)
  '(tool-bar-mode nil))
+;; '(confirm-kill-processes nil)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "FiraCode NF" :foundry "CTDB" :slant normal :weight normal :height 128 :width normal)))))
+ '(default ((t (:family "CaskaydiaCove Nerd Font Mono" :foundry "SAJA" :slant normal :weight normal :height 120 :width normal))))
+ '(mode-line ((t (:height 0.9))))
+ '(mode-line-inactive ((t (:height 0.9)))))
